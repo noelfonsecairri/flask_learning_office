bbccc79da1b4357d1bebc78f166266dec438aff8
noelfonsecairri <n.fonseca@irri.org>
2019-12-13 08:51:08 +0800

    added flask-related folders and files

---
+++ flask_learning_office.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+posts = [
+	{
+		'author': 'Noel Fonseca',
+		'title': 'Blog Post 1',
+		'content': 'First post content',
+		'date posted': 'December 12, 2019'
+	},
+	{
+		'author': 'Jane Doe',
+		'title': 'Blog Post 2',
+		'content': 'Second post content',
+		'date posted': 'December 13, 2019'
+	}
+]
+
+@app.route("/")
+@app.route("/home")
+def home():
+	return render_template('home.html', posts=posts)
+
+@app.route("/about")
+def about():
+	return render_template('about.html', title='About')
+
+if __name__ == "__main__":
+	app.run(debug=True)
