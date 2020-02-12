8eab218851b1bb994c1464ebbbb449f176b62ca3
noelfonsecairri <n.fonseca@irri.org>
2020-02-12 17:14:04 +0800

    re-watched the tutorial and recreated code

--- flask_learning_office.py
+++ flask_learning_office.py
@@ -1,18 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 posts = [
 	{
-		'author': 'Noel Fonseca',
+		'author': 'Corey Schafer',
 		'title': 'Blog Post 1',
 		'content': 'First post content',
-		'date posted': 'December 12, 2019'
+		'date_posted': 'April 20, 2018'
 	},
 	{
 		'author': 'Jane Doe',
 		'title': 'Blog Post 2',
 		'content': 'Second post content',
-		'date posted': 'December 13, 2019'
+		'date_posted': 'April 21, 2018'
 	}
 ]
 
@@ -20,6 +20,7 @@ posts = [
 @app.route("/home")
 def home():
 	return render_template('home.html', posts=posts)
+
 
 @app.route("/about")
 def about():
